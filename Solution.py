21de24368477cdb1e62afe7d0c557f91564d9fdc
SafeSorry <72054603+SafeSorry@users.noreply.github.com>
2022-09-05 10:18:03 -0400

    Create Solution.py

---
+++ Solution.py
@@ -0,0 +1,16 @@
+1. (10 pts) A hiring team has conducted interviews with 4 candidates and are now tasked with ranking the
+candidates. How many possible ways could they rank these candidates? Show all possible rankings using
+R/Python.
+
+2. (10 pts) It is getting cold in Ithaca and you have found your winter jacket from the depths of your closet. While
+checking your jacket’s pockets, you find that there are some coins in one of them. You see that you have a
+penny, nickel, dime, quarter, loonie ($1 coin), and a toonie ($2 coin) (because you are Canadian). If you were
+to put these coins back in your pocket and draw 3 at a time, how many different monetary sums are possible?
+(For ex: draw a loonie, toonie, and a penny = $3.01) Use code to show your numerical solution.
+
+3. (10 pts) How many ways can the word TOPICAL be arranged such that all the vowels are together? Use code
+to calculate your solution.
+
+4. (10 pts) Suppose you are planning a very cool block party with some of your house mates. You have 12 friends
+but you can only invite 7 of them. How many times will 3 particular friends always attend if you happen to
+invite them? Use code to support your numerical solution.
